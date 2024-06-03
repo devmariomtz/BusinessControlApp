3d3f899351dddfb1f282bcd40f04bd8e9f4f5249
mario-martinez-dev <143037183+mario-martinez-dev@users.noreply.github.com>
2024-06-03 15:02:15 -0600

    FIX: responsive in tables and theme set

---
+++ wwwroot/js/access.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    }
+});
